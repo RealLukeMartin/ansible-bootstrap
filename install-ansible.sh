2c446444af10e41f526a7398e71918e6ed815169
Luke Martin <lukemartinwebdev@gmail.com>
2016-05-24 10:56:51 -0500

    install pip and python-crypto

--- install-ansible.sh
+++ install-ansible.sh
@@ -34,12 +34,10 @@ if [ $? -eq 1 ]; then
   echo "Installing Ansible build dependencies."
   if [ -z $ANSIBLE_DEBUG ]; then
     apt-get -qq --force-yes update > /dev/null 2>&1
-    apt-get -qq --force-yes install git python-setuptools python-dev libffi-dev libssl-dev > /dev/null 2>&1
-    pip install pyopenssl
+    apt-get -qq --force-yes install git python-setuptools python-dev libffi-dev libssl-dev python-pip python-crypto -yq > /dev/null 2>&1
   else
     apt-get --force-yes update
-    apt-get --force-yes install git python-setuptools python-dev libffi-dev libssl-dev
-    pip install pyopenssl
+    apt-get --force-yes install git python-setuptools python-dev libffi-dev libssl-dev python-pip python-crypto -yq
   fi
 
   if [ -z $branch ] && [ ! -z $ANSIBLE_BRANCH ]; then
